b850ec507fbd2cd8170161563b150bb51cde1bcb
Kushagra Rai <krai_be24@thapar.edu>
2025-12-04 12:31:58 +0530

    Update Q3.cpp

--- Assignment-8/Q3.cpp
+++ Assignment-8/Q3.cpp
@@ -1,1 +1,95 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+Node* insert(Node* root, int val) {
+    if (root == NULL) {
+        return new Node(val);
+    }
+
+    if (val < root->data) {
+        root->left = insert(root->left, val);
+    } 
+    else if (val > root->data) {
+        root->right = insert(root->right, val);
+    } 
+    else {
+        cout << "Duplicate value " << val << " ignored." << endl;
+    }
+    return root;
+}
+Node* findMin(Node* node) {
+    Node* current = node;
+    while (current && current->left != NULL) {
+        current = current->left;
+    }
+    return current;
+}
+Node* deleteNode(Node* root, int key) {
+    if (root == NULL) return root;
+    if (key < root->data) {
+        root->left = deleteNode(root->left, key);
+    } 
+    else if (key > root->data) {
+        root->right = deleteNode(root->right, key);
+    } 
+    else {
+        if (root->left == NULL) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        } 
+        else if (root->right == NULL) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        Node* temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+int maxDepth(Node* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    int leftDepth = maxDepth(root->left);
+    int rightDepth = maxDepth(root->right);
+    
+    return 1 + max(leftDepth, rightDepth);
+}
+int minDepth(Node* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    if (root->left == NULL && root->right == NULL) {
+        return 1;
+    }
+    if (root->left == NULL) {
+        return 1 + minDepth(root->right);
+    }
+    if (root->right == NULL) {
+        return 1 + minDepth(root->left);
+    }
+    return 1 + min(minDepth(root->left), minDepth(root->right));
+}
+
+void inorder(Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
 
